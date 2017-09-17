4945de8ce6fd158d6aabc3c21192a07de33cf542
Shtuffy <joshoplano@gmail.com>
2017-09-17 15:21:52 -0500

    Update UniqueSymbolGenerator.java

--- SequenceAnalyzer/src/engine/UniqueSymbolGenerator.java
+++ SequenceAnalyzer/src/engine/UniqueSymbolGenerator.java
@@ -1,12 +1,31 @@
 package engine;
 
+import java.util.*;
+
 public class UniqueSymbolGenerator {
-	int symbolNum = 0;
+	private Map<String, String> varNames = new HashMap<>(); //Stores Variables and their values
+//	int symbolNum = 0;
 	
-	public String SymbolGenerator() {
-		String symbol = "_" + symbolNum;
-		symbolNum++;
-		return symbol;
+	//Generates a blank variable
+	public void Generate()
+	{
+		Generate("");
 	}
+	//Generates a variable with the given value
+	public void Generate(String input)
+	{
+		varNames.put(SymbolGen(), input);
+	}	
 	
+//	public String SymbolGenerator() {
+//		String symbol = "_" + symbolNum;
+//		symbolNum++;
+//		return symbol;
+//	}	
+	
+	//Creates next unique variable name
+	public String SymbolGen()
+	{
+		return "_Var" + varNames.size();
+	}	
 }
